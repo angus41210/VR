f7ca7b87cef164da23ba022f8632fbaadf42eace
angus41210 <160296017+angus41210@users.noreply.github.com>
2024-04-11 11:32:48 +0800

    VR2F3F

---
+++ library2F3F/data.js
@@ -0,0 +1,240 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "青少年悅讀區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.493280934844746,
+        "pitch": 0.11770701615701995,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6876988074029953,
+          "pitch": 0.22013806729755103,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.9773794140558003,
+          "pitch": 0.14881542606535092,
+          "title": "悅讀區",
+          "text": "有許多適合青少年閱讀的書籍"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "書櫃區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 2.688799402636959,
+        "pitch": 0.011019193588598597,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.526711712304195,
+          "pitch": 0.21992467726885323,
+          "rotation": 6.283185307179586,
+          "target": "2-"
+        },
+        {
+          "yaw": 3.0377751911598985,
+          "pitch": 0.22581467489892937,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.00707377125320896,
+          "pitch": 0.24310586955524371,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.9067363748515733,
+          "pitch": 0.06980179151150523,
+          "title": "書庫",
+          "text": "擁有各類豐富的藏書"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "閱覽區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 2.8954093003542676,
+        "pitch": 0.06489230972792726,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.18998317497586115,
+          "pitch": 0.1888799920548596,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.6855878405477256,
+          "pitch": 0.010435576231783372,
+          "title": "閱報室",
+          "text": "主要提供報紙閱讀"
+        },
+        {
+          "yaw": -0.7331739895168266,
+          "pitch": -0.02575011371482816,
+          "title": "影印室",
+          "text": "席供影印服務"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "二樓天窗",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -2.1441037886389136,
+        "pitch": -0.2728214227858814,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4421678038807126,
+          "pitch": 0.17272519291918798,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -1.2988001717201723,
+          "pitch": -0.5743920025392111,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.8140416138719146,
+          "pitch": -0.00024411291154535775,
+          "title": "天窗",
+          "text": "能夠引入光線，連通上下"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "三樓中庭",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.5321691574658267,
+        "pitch": 0.06028503772806815,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2775098904855273,
+          "pitch": 0.11148854128002128,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6938720769898268,
+          "pitch": 0.26270286777685214,
+          "title": "中庭",
+          "text": "能夠看到大廳、二樓至四樓"
+        }
+      ]
+    }
+  ],
+  "name": "library2f3f",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
