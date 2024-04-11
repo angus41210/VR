c95ed5fb80b1bb7193c9a1e9a52a10822e468656
angus41210 <160296017+angus41210@users.noreply.github.com>
2024-04-11 11:30:25 +0800

    VR files

---
+++ library1F/data.js
@@ -0,0 +1,340 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "正門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.3075646038194044,
+        "pitch": 0.026969864934542898,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.05874365873940057,
+          "pitch": 0.0242412869100459,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 1.321908637815314,
+          "pitch": 0.13767927616995124,
+          "rotation": 7.0685834705770345,
+          "target": "5-"
+        },
+        {
+          "yaw": -0.012654166194369765,
+          "pitch": 0.519518293274853,
+          "rotation": 3.141592653589793,
+          "target": "6-mrt"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.27866275948437647,
+          "pitch": -0.1641625551886321,
+          "title": "國立台灣圖書館",
+          "text": "位於位於新北市中和區四號公園，是一所兼具大眾與學術取向的公共圖書館"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "大廳",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.3467675842379023,
+        "pitch": 0.0035258498077972433,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6004428177340451,
+          "pitch": 0.5035795079644068,
+          "rotation": 3.141592653589793,
+          "target": "0-"
+        },
+        {
+          "yaw": -0.9015769314465221,
+          "pitch": 0.13247886960620114,
+          "rotation": 5.497787143782138,
+          "target": "3-"
+        },
+        {
+          "yaw": 0.4442178907571481,
+          "pitch": 0.07810420409164287,
+          "rotation": 13.351768777756625,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.06685858862943128,
+          "pitch": -0.04615953364221781,
+          "title": "服務台",
+          "text": "位於圖書館大廳的服務台"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "期刊室",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.6861651467448286,
+        "pitch": 0.13294545321636164,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7011770108357434,
+          "pitch": 0.662968701130346,
+          "rotation": 3.9269908169872414,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.142828492910283,
+          "pitch": 0.08565755404950615,
+          "title": "報架",
+          "text": "提供報紙和期刊閱讀"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "一樓樓梯",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 2.803947798327763,
+        "pitch": -0.07089397391589713,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8543230301823472,
+          "pitch": 0.09674213656469988,
+          "rotation": 0.7853981633974483,
+          "target": "1-"
+        },
+        {
+          "yaw": -1.6886542680992846,
+          "pitch": 0.19492840716342208,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.9588736871810184,
+          "pitch": 0.07031442099821206,
+          "title": "書架",
+          "text": "一樓的書價與書櫃"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "咖啡館",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.1951961462079286,
+        "pitch": 0.09685318663905207,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.20610728507460152,
+          "pitch": 0.07612175677089184,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.443668284005331,
+          "pitch": 0.0793704180456345,
+          "title": "布納咖啡館",
+          "text": "位於圖書館附近的咖啡廳"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "餐廳大廳",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.4013767056135471,
+        "pitch": 0.14924444602677056,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.22752512298383287,
+          "pitch": 0.36162179778427017,
+          "rotation": 3.141592653589793,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6414956628572366,
+          "pitch": 0.1083710610569355,
+          "title": "水源會館",
+          "text": "同樣位於四號公園的餐廳"
+        }
+      ]
+    },
+    {
+      "id": "6-mrt",
+      "name": "永安市場MRT",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 1.6216508340032458,
+        "pitch": -0.10442950109596438,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3481852787054223,
+          "pitch": 0.38548630738937995,
+          "rotation": 3.141592653589793,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.4728179998406166,
+          "pitch": -0.21934233893073163,
+          "title": "捷運永安市場站",
+          "text": "屬於中和新蘆線的捷運站"
+        }
+      ]
+    }
+  ],
+  "name": "library1F",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
