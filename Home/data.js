140b4ab5349e35ec12a634bfb9c363a965c6a440
angus41210 <160296017+angus41210@users.noreply.github.com>
2024-04-11 11:41:37 +0800

    Home

---
+++ Home/data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "正門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.2931460065327638,
+          "pitch": -0.23488839649365012,
+          "title": "國立台灣圖書館",
+          "text": "位於位於新北市中和區四號公園，是一所兼具大眾與學術取向的公共圖書館"
+        }
+      ]
+    }
+  ],
+  "name": "home",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
